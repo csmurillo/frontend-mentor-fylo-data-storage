610ef6aa36d1ca5933c08d156f7e66ecd1fa3442
csmurillo <csmurillo00@gmail.com>
2022-03-08 22:19:37 -0800

    Updates

--- _next/static/WnSTM_gPhpxS41K3QPA31/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/716-a09d07c22241a2f8.js","static/chunks/pages/index-54fec029a8a12e24.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
