9f945003a30459e6ae01a70284a9be7c3e6e0695
csmurillo <csmurillo00@gmail.com>
2022-03-08 21:43:33 -0800

    Updates

--- _next/static/IboxnWAAy2mrXoTtwbOhj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/716-a09d07c22241a2f8.js","static/chunks/pages/index-f262659d6fdda504.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
